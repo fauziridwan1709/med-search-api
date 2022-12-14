5fcddd53c962fe39de01a76babbafa7a4e273ccd
Muhamad Fauzi Ridwan <evan1001t@gmail.com>
2022-12-14 23:21:35 +0700

    feat: resolve CORS

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from search import BSBI_instance
  
 app = Flask(__name__)
+CORS(app, support_credentials=True)
  
 @app.route("/")
+@cross_origin(supports_credentials=True)
 def home_view():
         return "<h1>Welcome to good med search service</h1>"
 
 @app.route("/search")
+@cross_origin(supports_credentials=True)
 def search():
         args_dict = request.args.to_dict()
         query = args_dict.get("q")
