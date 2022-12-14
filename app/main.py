1158f1fd1b5b90c1f3dff34d6563b571e85585ce
Muhamad Fauzi Ridwan <evan1001t@gmail.com>
2022-12-14 23:27:54 +0700

    feat: resolve CORS

--- app/main.py
+++ app/main.py
@@ -3,15 +3,13 @@ from flask_cors import CORS, cross_origin
 from search import BSBI_instance
  
 app = Flask(__name__)
-CORS(app, support_credentials=True)
+CORS(app)
  
 @app.route("/")
-@cross_origin(supports_credentials=True)
 def home_view():
         return "<h1>Welcome to good med search service</h1>"
 
 @app.route("/search")
-@cross_origin(supports_credentials=True)
 def search():
         args_dict = request.args.to_dict()
         query = args_dict.get("q")
@@ -19,4 +17,6 @@ def search():
         result = []
         for (score, doc) in tfidf:
                 result.append(doc)
-        return jsonify(result)
+        response = jsonify(result)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
