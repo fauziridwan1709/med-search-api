efe9f3041f83c64c53fb4982f12fb78712bf7735
Muhamad Fauzi Ridwan <evan1001t@gmail.com>
2022-12-14 21:58:30 +0700

    feat: retrieve query

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from search import BSBI_instance
  
 app = Flask(__name__)
  
 @app.route("/")
 def home_view():
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+        return "<h1>Welcome to good med search service</h1>"
+
+@app.route("/search")
+def search():
+        args_dict = request.args.to_dict()
+        query = args_dict.get("q")
+        tfidf = BSBI_instance.retrieve_tfidf(query, k = 10)
+        return jsonify(tfidf)
